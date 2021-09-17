b6f06aeaa7cd076e8c9694af2064fa87f412e809
chuchiss <87209644+chuchiss@users.noreply.github.com>
2021-09-17 04:14:41 -0300

    Update trabajo.js

--- plugins/trabajo.js
+++ plugins/trabajo.js
@@ -1,16 +1,6 @@
 let handler = async m => m.reply(`
 ▫️👉[empleo 1]
-LABURA desde CASA
-Regístrate y gana de 100 a 300 pesos
-reales mirando vídeos. 
-▫️OPORTUNIDAD de ganar 
-hasta 8 mil pesos semanale$$
-▫️Registrate con el codigo
-https://s.kwai.app/s/odEMde9C
-*Luego cuando finalizes de registrarte*
-*pone .verificar*
-*para ganar premium en InsaneBOT*
-_______________________________
+ACTUALMENTE NO DISPONIBLE
 
 `.trim()) 
 handler.help = ['trabajo','premium']
