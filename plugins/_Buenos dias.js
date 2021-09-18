0f672740448c367724c21fd485a88c9a8ee3aad2
tenquius <87209189+tenquius@users.noreply.github.com>
2021-09-18 06:19:12 -0300

    Delete plugins directory

--- plugins/_Buenos dias.js
+++
@@ -1,7 +0,0 @@
-let handler  = async (m, { conn }) => {
-       conn.sendFile(m.chat, 'media/Buenos días.mp3', '', 'xd', m)
-}
-handler.command = /^(bd)$/i
-handler.fail = null
-handler.exp = 100
-module.exports = handler
